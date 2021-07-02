8791d5cfb06a030a04d54cf3c2bfc4b293ed999c
AnaSokol <86832525+AnaSokol@users.noreply.github.com>
2021-07-02 15:36:31 +0300

    Update CheckoutPage.java

--- src/main/java/pages/CheckoutPage.java
+++ src/main/java/pages/CheckoutPage.java
@@ -15,19 +15,6 @@ public class CheckoutPage extends BasePage {
     @FindBy(xpath = "//button[@type='submit']")
     private WebElement addMyEmailButton;
 
-
-//    @FindBy(xpath = "//button[contains(@class,'choose-payment-method__btn--regular-card')]")
-//    private WebElement paymentCartButton;
-//
-//    @FindBy(xpath = "//*[@class='opc-billing-form']/*[@class='opc-billing-form__wrapper']")
-//    private WebElement billingForm;
-//
-//    @FindBy(xpath = "//div[@class='checkout-payment-form__wrapper']")
-//    private WebElement paymentForm;
-//
-//    @FindBy(xpath = "//button[contains(@class,'checkout-order-summary__continue-btn')]")
-//    private WebElement completeOrderButton;
-
     public CheckoutPage(WebDriver driver) {
         super(driver);
     }
@@ -42,20 +29,5 @@ public class CheckoutPage extends BasePage {
     public void clickAddMyEmailButton() {
         addMyEmailButton.click();
     }
-//    public void clickPaymentCartButton() {
-//        paymentCartButton.click();
-//    }
-//
-//    public boolean isBillingFormVisible() {
-//        return billingForm.isDisplayed();
-//    }
-//
-//    public boolean isPaymentFormVisible() {
-//        return paymentForm.isDisplayed();
-//    }
-//
-//    public boolean isCompleteOrderButtonVisible() {
-//        return completeOrderButton.isDisplayed();
-//    }
-//}
+
 }
